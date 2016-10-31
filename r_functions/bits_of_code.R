576d1b75e8bc34130727f7b43bb65987e91bc741
Dylan Childs <d.childs@sheffield.ac.uk>
2016-10-31 22:23:34 +0000

    ANOVA

--- r_functions/bits_of_code.R
+++ r_functions/bits_of_code.R
@@ -1,32 +1,12 @@
-#
-set.seed(27081975)
-samp_size <- 250
-plant_morphs <- sample(c("Purple","Green"), samp_size, replace = TRUE, prob = c(4,6))
-mns <- c(Purple = 760, Green = 700)[plant_morphs]
-sds <- c(Purple = 160, Green = 150)[plant_morphs]
+library(multcomp)
 
-morph.data <- data.frame(Colour = plant_morphs,
-                         Weight = rnorm(samp_size, mns, sds))
+diet <- read.csv(file = "./data_csv/DIET_EFFECTS.CSV")
 
-t.test(Weight ~ Colour, data = morph.data)
+amod <- aov(WeightLoss ~ Plan, data = diet)
 
-plant_morphs <- morph.data$Colour
-samp_size <- length(plant_morphs)
-n_samp <- 10000
-out <- replicate(n_samp, {
-  samp <- sample(plant_morphs, replace = TRUE)
-  sum(samp == "Purple") / samp_size
-})
+tuk <- glht(amod, linfct = mcp(Plan = "Tukey"))
 
-hist(out)
+summary(tuk)
 
-sum(out < 0.25) / n_samp
-
-set.seed(123)
-x<-rnorm(1000)
-x2<-x/3
-sd(x)
-sd(x2)
-sd(x)/3
-
-sd(x)/3 == sd(x2) where x2 is x/3
+tuk.cld <- cld(tuk, decreasing = FALSE)
+plot(tuk.cld)
